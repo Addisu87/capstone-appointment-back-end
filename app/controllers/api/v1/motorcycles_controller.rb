3b8d8befe1686c467ed7d2213be382c01ad30179
Addisu Haile Tedla <addisu.haile@yahoo.com>
2022-10-20 20:14:52 +0300

    Update the reservation controller

--- app/controllers/api/v1/motorcycles_controller.rb
+++ app/controllers/api/v1/motorcycles_controller.rb
@@ -1,4 +1,7 @@
 class Api::V1::MotorcyclesController < ApplicationController
+  before_action :set_motorcycle, only: %i[show edit update destroy]
+  load_and_authorize_resource
+
   # GET /motorcycles or /motorcycles.json
   def index
     @motorcycles = Motorcycle.where(user_id: current_user.id).order(created_at: :desc)
@@ -26,7 +29,7 @@ class Api::V1::MotorcyclesController < ApplicationController
 
     respond_to do |format|
       if @motorcycle.save
-        format.html { redirect_to api_v1_motorcycles(@motorcycle), notice: 'Motorcycle was successfully created.' }
+        format.html { redirect_to api_v1_motorcycles_url(@motorcycle), notice: 'Motorcycle was successfully created.' }
         format.json { render :show, status: :created, location: @motorcycle }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -40,7 +43,7 @@ class Api::V1::MotorcyclesController < ApplicationController
     set_motorcycle
     respond_to do |format|
       if @motorcycle.update(motorcycle_params)
-        format.html { redirect_to api_v1_motorcycle(@motorcycle), notice: 'Motorcycle was successfully updated.' }
+        format.html { redirect_to api_v1_motorcycle_url(@motorcycle), notice: 'Motorcycle was successfully updated.' }
         format.json { render :show, status: :ok, location: @motorcycle }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -54,7 +57,7 @@ class Api::V1::MotorcyclesController < ApplicationController
     @motorcycle.destroy
 
     respond_to do |format|
-      format.html { redirect_to api_v1_motorcycle, notice: 'Motorcycle was successfully destroyed.' }
+      format.html { redirect_to api_v1_motorcycle_url, notice: 'Motorcycle was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -1,22 +1,80 @@
 class Api::V1::ReservationsController < ApplicationController
+  before_action :set_reservation, only: %i[show edit update destroy]
+  load_and_authorize_resource
+
+  # GET /reservations or /reservations.json
   def index
+    @reservations = Reservation.where(user_id: current_user.id).order(created_at: :desc)
   end
 
-  def new
-  end
-
-  def create
-  end
-
-  def edit
-  end
-
+  # GET /reservations/1 or /reservations/1.json
   def show
+    set_reservation
   end
 
+  # GET /reservations/new
+  def new
+    @reservation = Reservation.new
+  end
+
+  # GET /reservations/1/edit
+  def edit
+    set_reservation
+  end
+
+  # POST /reservations or /reservations.json
+  def create
+    @reservation = Reservation.new(reservation_params)
+    @reservation.user_id = current_user.id
+
+    respond_to do |format|
+      if @reservation.save
+        format.html do
+          redirect_to api_v1_reservations_url(@reservation), notice: 'Reservation was successfully created.'
+        end
+        format.json { render :show, status: :created, location: @reservation }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @reservation.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /reservations/1 or /reservations/1.json
   def update
+    set_reservation
+    respond_to do |format|
+      if @reservation.update(reservation_params)
+        format.html do
+          redirect_to api_v1_reservation_url(@reservation), notice: 'Reservation was successfully updated.'
+        end
+        format.json { render :show, status: :ok, location: @reservation }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @reservation.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
+  # DELETE /reservations/1 or /reservations/1.json
   def destroy
+    @reservation.destroy
+
+    respond_to do |format|
+      format.html { redirect_to api_v1_reservation_url, notice: 'reservation was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_reservation
+    @reservation = Reservation.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def reservation_params
+    params.require(:reservation).permit(:city, :date)
   end
 end
