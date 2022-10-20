756aedaaa1bb24c3ce176522f8dff4af4848f7a9
Addisu Haile Tedla <addisu.haile@yahoo.com>
2022-10-20 18:25:21 +0300

    Updates the routes configarations

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get 'reservations/index'
-      get 'reservations/new'
-      get 'reservations/create'
-      get 'reservations/edit'
-      get 'reservations/show'
-      get 'reservations/update'
-      get 'reservations/destroy'
-      get 'motorcycles/index'
-      get 'motorcycles/new'
-      get 'motorcycles/create'
-      get 'motorcycles/edit'
-      get 'motorcycles/show'
-      get 'motorcycles/update'
-      get 'motorcycles/destroy'
-      get 'sessions/create'
-      get 'registrations/new'
-      get 'registrations/create'
+      resources :motorcycles
+      resources :reservations
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
