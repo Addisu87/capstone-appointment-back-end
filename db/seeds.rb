57be3f587c92402a141439eaff2121b0ad2dedb8
Addisu Haile Tedla <addisu.haile@yahoo.com>
2022-10-21 21:11:08 +0300

    Update seed files

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,41 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-user1 = User.new(name: 'Addisu')
-user1.save
+
+
+# user
+User.create(
+  name: 'Addisu',
+)
+
+# motorcycles
+Motorcycle.create(
+  model: 'Neiman Marcus Limited Edition Fighter', 
+  avatar:'https://cdn.wealthygorilla.com/wp-content/uploads/2019/06/Most-Expensive-Motorbikes-Neiman-Marcus-Limited-Edition-Fighter.webp',
+  duration: 48,
+  price: 11000000,
+  description: 'The Neiman Marcus Limited Edition Fighter is an exclusive streetfighter motorcycle by the American Luxury Department Store Brand. It draws its power from an air-cooled V-Twin engine that propels it to a top speed of 300 km/h making it one of the fastest bikes in the world.')
+
+Motorcycle.create(
+  modle: 'Ecosse ES1 Spirit',
+  avatar: 'https://cdn.wealthygorilla.com/wp-content/uploads/2019/06/Most-Expensive-Motorbikes-Ecosse-ES1-Spirit.webp',
+  duration: 50,
+  price: 7000000,
+  description: 'The Ecosse Spirit is a limited edition titanium motorcycle and this bike packs enough power to satisfy even the most experienced rider, reaching speeds of 370 kilometres.'
+)
+
+# reservations
+Reservation.create(
+  date: '2022-12-01',
+  city: 'Gondar',
+  motorcycle_id: 1,
+  user_id: 1,
+);
+
+Reservation.create(
+  date: '2022-12-02',
+  city: 'Addis Ababa',
+  motorcycle_id: 2,
+  user_id: 1,
+);
+
